d8f241159ba0482d261b0f7d41bf4da85efac016
Pawan Vishwakarma <pawanvishwakarma31577@gmail.com>
2025-05-27 18:22:31 +0530

    added disjoint set union

---
+++ Graph/4-Mini-Spanning-Tree/G-36-disjoint-set-union.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+class DisjointSet{
+    vector<int> rank;
+    vector<int> parent;
+    
+    public:
+        DisjointSet(int n){
+            // n+1 for 1 based indexing
+            rank.resize(n+1, 0);
+            parent.resize(n+1, 0);
+            
+            // initiate with everyone is their own parent
+            for(int i=0; i<=n; i++){
+                parent[i]=i;
+            }
+        }
+        
+        int findPar(int n){
+            if(parent[n] == n){
+                return n;
+            }
+            //return findPar(parent[n]); //array will make path for parents
+            return parent[n] = findPar(parent[n]); // path compression 
+            
+        }
+        
+        void unionByRank(int u, int v){
+            // find parent of u, v
+            int upar= findPar(u);
+            int vpar= findPar(v);
+            if(upar == vpar)return; // same parent part of same component
+            
+            // merge the u or v based on their parents rank
+            if(rank[upar] < rank[vpar]){
+                parent[upar]= vpar;
+                
+            }
+            else if(rank[upar] > rank[vpar]){
+                parent[vpar]= upar;
+            }
+            else{
+                // merge v to u
+                parent[vpar]= upar;
+                // increase rank of u
+                rank[upar]++;
+            }
+            
+        }
+};
+
+
+int main() {
+	DisjointSet ds(7);
+	
+	ds.unionByRank(1,2);
+	ds.unionByRank(2,3);
+	ds.unionByRank(4,5);
+	ds.unionByRank(6,7);
+	ds.unionByRank(5,6);
+	
+	if(ds.findPar(3) == ds.findPar(7)){
+	    cout<<"same"<<endl;
+	}
+	else{
+	   cout<<"Not same"<<endl;
+	}
+	
+	ds.unionByRank(3,7);
+	
+	if(ds.findPar(3) == ds.findPar(7)){
+	    cout<<"same"<<endl;
+	}
+	else{
+	   cout<<"Not same"<<endl;
+	}
+	return 0;
+
+}
