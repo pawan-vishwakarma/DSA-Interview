3d3566e788db294775e2629810dcc2d789b1ed16
Pawan Vishwakarma <pawanvishwakarma31577@gmail.com>
2025-06-12 14:55:54 +0530

    bridges in grpah code

---
+++ Graph/5-Other-algo/G-45-bridges-tarjan's-algo.cpp
@@ -0,0 +1,45 @@
+// refer- https://leetcode.com/problems/critical-connections-in-a-network/submissions/1661746435/
+
+class Solution {
+private:
+    int timer=1;
+    void dfs(int node, int parent, vector<int> adj[], 
+            vector<int> &vis, int time[], int low[], vector<vector<int>> &bridges){
+        vis[node]=1;
+        time[node]= low[node]= timer;
+        timer++;
+
+        for(auto it: adj[node]){
+            if(it == parent) continue;
+            if(vis[it] == 0){ // not visieted
+                dfs(it, node, adj, vis, time, low, bridges);
+                low[node]= min(low[node], low[it]);
+                // check for bridge
+                if(low[it] > time[node]){
+                    bridges.push_back({it, node});
+                }
+            }
+            else{
+                low[node]= min(low[node], low[it]);
+            }
+        }
+    }
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for(auto it: connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> vis(n, 0);
+        int time[n];
+        int low[n];
+        vector<vector<int>> bridges;
+
+        dfs(0, -1, adj, vis, time, low, bridges);
+
+        return bridges;
+
+    }
+};
